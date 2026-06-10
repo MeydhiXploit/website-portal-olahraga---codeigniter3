57a4eeec64548bdaa7bb707f2159a28aca7d20c8
MEYDHIARINUGROHO <meydhiarinugroho@example.com>
2026-06-10 14:54:11 +0800

    feat login

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,7 +54,8 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 // USER
-$route['login'] = 'UserController/loginAdmin';
+$route['login'] = 'UserController/loginUser';
+$route['register'] = 'UserController/register';
 $route['logout'] = 'UserController/logout';
 $route['news/(:any)'] = 'NewsController/news_page';
 $route['sport/(:any)'] = 'SportController/sport';
